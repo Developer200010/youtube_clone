5edf0d5af0990714af8a6a1d2bf4dd90cb177d3f
chandan vishwakarma <chandanvishw11002@gmail.com>
2025-09-08 01:30:52 +0530

    signup and login funtionality with require packages like bcrypt and jwt is done

--- server.js
+++ server.js
@@ -1,22 +1,20 @@
 import express from "express";
-import mongoose from "mongoose";
 import dotenv from "dotenv";
 import cors from "cors";
+import connectDB from "./config/db.js";
+
+// Routes
+import authRoutes from "./routes/authRoutes.js";
 
 dotenv.config();
-const app = express();
+connectDB();
 
-// middlewares
+const app = express();
 app.use(cors());
 app.use(express.json());
 
-
-mongoose.connect(process.env.MONGO_URL)
-.then(()=>console.log("db is connected successfully"))
-.catch((err)=> console.log(err.message));
+// Routes
+app.use("/api/auth", authRoutes);
 
 const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, ()=>{
-    console.log(`server is running on port${PORT}`)
-})
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
